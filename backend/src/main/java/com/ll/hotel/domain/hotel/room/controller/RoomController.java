e6a8a108ef0ed742ec4f02a8235a49736812be9c
Gyeonghun Min <riverpower6@naver.com>
2025-02-02 02:50:04 +0900

    [feat] 객실 추가

--- backend/src/main/java/com/ll/hotel/domain/hotel/room/controller/RoomController.java
+++ backend/src/main/java/com/ll/hotel/domain/hotel/room/controller/RoomController.java
@@ -1,8 +1,15 @@
 package com.ll.hotel.domain.hotel.room.controller;
 
+import com.ll.hotel.domain.hotel.room.dto.PostRoomRequest;
+import com.ll.hotel.domain.hotel.room.dto.PostRoomResponse;
 import com.ll.hotel.domain.hotel.room.service.RoomService;
+import com.ll.hotel.global.rsData.RsData;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,4 +19,14 @@ import org.springframework.web.bind.annotation.RestController;
 @Tag(name = "RoomController", description = "객실 컨트롤러")
 public class RoomController {
     private final RoomService roomService;
+
+    @PostMapping
+    public RsData<PostRoomResponse> roomCreate(@PathVariable long hotelId,
+                                               @RequestBody @Valid PostRoomRequest postRoomRequest) {
+        return new RsData<>(
+                "201-1",
+                "객실을 추가하였습니다.",
+                this.roomService.create(hotelId, postRoomRequest)
+        );
+    }
 }
--- backend/src/main/java/com/ll/hotel/domain/hotel/room/service/RoomService.java
+++ backend/src/main/java/com/ll/hotel/domain/hotel/room/service/RoomService.java
@@ -3,9 +3,39 @@ package com.ll.hotel.domain.hotel.room.service;
 import com.ll.hotel.domain.hotel.room.repository.RoomRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import com.ll.hotel.domain.hotel.hotel.entity.Hotel;
+import com.ll.hotel.domain.hotel.hotel.repository.HotelRepository;
+import com.ll.hotel.domain.hotel.room.dto.PostRoomRequest;
+import com.ll.hotel.domain.hotel.room.dto.PostRoomResponse;
+import com.ll.hotel.domain.hotel.room.entity.Room;
+import com.ll.hotel.domain.hotel.room.type.BedTypeNumber;
+import com.ll.hotel.global.exceptions.ServiceException;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class RoomService {
+    private final HotelRepository hotelRepository;
     private final RoomRepository roomRepository;
+
+    @Transactional
+    public PostRoomResponse create(long hotelId, PostRoomRequest postRoomRequest) {
+        Hotel hotel = this.hotelRepository.findById(hotelId)
+                .orElseThrow(() -> new ServiceException("404-1", "호텔의 정보가 존재하지 않습니다."));
+
+        BedTypeNumber bedTypeNumber = BedTypeNumber.fromJson(postRoomRequest.bedTypeNumber());
+
+        Room room = Room.builder()
+                .hotel(hotel)
+                .roomName(postRoomRequest.roomName())
+                .roomNumber(postRoomRequest.roomNumber())
+                .basePrice(postRoomRequest.basePrice())
+                .standardNumber(postRoomRequest.standardNumber())
+                .maxNumber(postRoomRequest.maxNumber())
+                .bedTypeNumber(bedTypeNumber)
+                // 객실 옵션 추가
+                .build();
+
+        return new PostRoomResponse(this.roomRepository.save(room));
+    }
 }
