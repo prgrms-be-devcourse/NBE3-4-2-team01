f0b4416ccca1d33cc295ff806783c3214131a2ec
universechoi <skye.of.universe@gmail.com>
2025-02-03 23:35:55 +0900

    [chore] 병합 충돌 방지를 위해 파일 수정

--- backend/src/main/java/com/ll/hotel/domain/member/member/repository/BusinessRepository.java
+++ backend/src/main/java/com/ll/hotel/domain/member/member/repository/BusinessRepository.java
@@ -2,8 +2,6 @@ package com.ll.hotel.domain.member.member.repository;
 
 import com.ll.hotel.domain.member.member.entity.Business;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface BusinessRepository extends JpaRepository<Business, Long> {
 }
