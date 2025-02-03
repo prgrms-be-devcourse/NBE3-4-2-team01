22bdce774946e938f839e880124569060eef2797
universechoi <skye.of.universe@gmail.com>
2025-02-03 23:20:15 +0900

    [refactor] 응답 구조를 변경하고 반환 타입 수정

--- backend/src/main/java/com/ll/hotel/domain/member/admin/controller/AdminBusinessController.java
+++ backend/src/main/java/com/ll/hotel/domain/member/admin/controller/AdminBusinessController.java
@@ -1,6 +1,5 @@
 package com.ll.hotel.domain.member.admin.controller;
 
-import com.ll.hotel.domain.member.admin.dto.AdminBusinessDto;
 import com.ll.hotel.domain.member.admin.dto.request.AdminBusinessRequest;
 import com.ll.hotel.domain.member.admin.dto.response.AdminBusinessResponse;
 import com.ll.hotel.domain.member.admin.service.AdminBusinessService;
@@ -19,37 +18,37 @@ public class AdminBusinessController {
     private final AdminBusinessService adminBusinessService;
 
     @GetMapping
-    public RsData<Page<AdminBusinessDto>> getAll(
+    public RsData<Page<AdminBusinessResponse.Summary>> getAll(
             @RequestParam(value = "page", defaultValue = "0") int page) {
 
-        Page<AdminBusinessDto> pagedAdminBusinessDto = adminBusinessService.findAllPaged(page)
-                .map(AdminBusinessDto::toDto);
+        Page<AdminBusinessResponse.Summary> pagedBusinessSummaries = adminBusinessService.findAllPaged(page)
+                .map(AdminBusinessResponse.Summary::from);
 
-        if (!pagedAdminBusinessDto.hasContent()) {
+        if (!pagedBusinessSummaries.hasContent()) {
             throw new ServiceException("404", "요청하신 사업자 정보 페이지가 없습니다.");
         }
 
         return new RsData<>(
                 "200",
                 "모든 사업자 정보가 조회되었습니다.",
-                pagedAdminBusinessDto
+                pagedBusinessSummaries
         );
     }
 
     @GetMapping("/{id}")
-    public RsData<AdminBusinessDto> getById(@PathVariable("id") Long id) {
+    public RsData<AdminBusinessResponse.Detail> getById(@PathVariable("id") Long id) {
 
         Business business = adminBusinessService.findById(id);
 
         return new RsData<>(
                 "200",
                 "사업자 정보가 조회되었습니다.",
-                AdminBusinessDto.toDto(business)
+                AdminBusinessResponse.Detail.from(business)
         );
     }
 
     @PatchMapping("/{id}")
-    public RsData<AdminBusinessResponse> approve(@PathVariable("id") Long id,
+    public RsData<AdminBusinessResponse.ApprovalResult> approve(@PathVariable("id") Long id,
                                                  @RequestBody @Valid AdminBusinessRequest adminBusinessRequest) {
         Business business = adminBusinessService.findById(id);
 
@@ -60,7 +59,7 @@ public class AdminBusinessController {
         return new RsData<>(
                 "200",
                 "사업자 승인 정보가 수정되었습니다.",
-                new AdminBusinessResponse(business)
+                AdminBusinessResponse.ApprovalResult.from(business)
         );
     }
 }
--- backend/src/main/java/com/ll/hotel/domain/member/admin/dto/response/AdminBusinessResponse.java
+++ backend/src/main/java/com/ll/hotel/domain/member/admin/dto/response/AdminBusinessResponse.java
@@ -1,13 +1,68 @@
 package com.ll.hotel.domain.member.admin.dto.response;
 
+import com.ll.hotel.domain.member.member.dto.MemberDto;
 import com.ll.hotel.domain.member.member.entity.Business;
+import com.ll.hotel.domain.member.member.entity.Member;
+import com.ll.hotel.domain.member.member.entity.Role;
 import com.ll.hotel.domain.member.member.type.BusinessApprovalStatus;
+import com.ll.hotel.domain.member.member.type.MemberStatus;
+import jakarta.persistence.Column;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 
-public record AdminBusinessResponse(
-        String businessRegistrationNumber,
-        BusinessApprovalStatus approvalStatus
-) {
-    public AdminBusinessResponse(Business business) {
-        this(business.getBusinessRegistrationNumber(), business.getApprovalStatus());
+import java.time.LocalDate;
+
+public class AdminBusinessResponse {
+    public record ApprovalResult(
+            String businessRegistrationNumber,
+            BusinessApprovalStatus approvalStatus
+    ) {
+        public static ApprovalResult from(Business business) {
+            return new ApprovalResult(
+                    business.getBusinessRegistrationNumber(),
+                    business.getApprovalStatus()
+            );
+        }
+    }
+    public record Summary(
+            Long id,
+            String businessRegistrationNumber,
+            BusinessApprovalStatus approvalStatus
+    ) {
+        public static Summary from(Business business) {
+            return new Summary(
+                    business.getId(),
+                    business.getBusinessRegistrationNumber(),
+                    business.getApprovalStatus()
+            );
+        }
+    }
+
+    public record Detail(
+            Long businessId,
+            String businessRegistrationNumber,
+            BusinessApprovalStatus approvalStatus,
+
+            Long userId,
+            String name,
+            String email,
+            String phoneNumber,
+            LocalDate birth,
+            MemberStatus status
+    ) {
+        public static Detail from(Business business) {
+            Member owner = business.getMember();
+            return new Detail(
+                    business.getId(),
+                    business.getBusinessRegistrationNumber(),
+                    business.getApprovalStatus(),
+                    owner.getId(),
+                    owner.getMemberName(),
+                    owner.getMemberEmail(),
+                    owner.getMemberPhoneNumber(),
+                    owner.getBirthDate(),
+                    owner.getMemberStatus()
+            );
+        }
     }
 }
