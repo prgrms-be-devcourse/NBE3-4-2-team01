bd658ab9ea979686671ff21dfba1eb6ca72a00bf
jurio5 <sia5492@naver.com>
2025-02-03 18:46:13 +0900

    [feat] 즐겨찾기 관련 DB 작업 인터페이스 추가

---
+++ backend/src/main/java/com/ll/hotel/domain/member/favorite/repository/FavoriteRepository.java
@@ -0,0 +1,9 @@
+package com.ll.hotel.domain.member.favorite.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.ll.hotel.domain.member.favorite.entity.Favorite;
+
+public interface FavoriteRepository extends JpaRepository<Favorite, Long> {
+    Favorite findByHotelIdAndMemberId(String hotelId, String memberId);
+}
