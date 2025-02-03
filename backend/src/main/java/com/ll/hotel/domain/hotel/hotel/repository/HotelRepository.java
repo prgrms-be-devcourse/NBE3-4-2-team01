5d1e19b4c25364212beeba43b4aa07b98238166a
universechoi <skye.of.universe@gmail.com>
2025-02-03 17:18:39 +0900

    [chore] 병합 충돌 방지를 위해 파일 수정

--- backend/src/main/java/com/ll/hotel/domain/hotel/hotel/repository/HotelRepository.java
+++ backend/src/main/java/com/ll/hotel/domain/hotel/hotel/repository/HotelRepository.java
@@ -1,9 +1,4 @@
 package com.ll.hotel.domain.hotel.hotel.repository;
 
-import com.ll.hotel.domain.hotel.hotel.entity.Hotel;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface HotelRepository extends JpaRepository<Hotel, Long> {
+public class HotelRepository {
 }
