6f645c81000ea618f21e839413e8e7e29a06da3e
universechoi <skye.of.universe@gmail.com>
2025-02-03 23:11:19 +0900

    [refactor] 응답 구조를 변경하고 반환 타입 수정

--- backend/src/main/java/com/ll/hotel/domain/member/admin/controller/AdminHotelController.java
+++ backend/src/main/java/com/ll/hotel/domain/member/admin/controller/AdminHotelController.java
@@ -1,7 +1,6 @@
 package com.ll.hotel.domain.member.admin.controller;
 
 import com.ll.hotel.domain.hotel.hotel.entity.Hotel;
-import com.ll.hotel.domain.member.admin.dto.AdminHotelDto;
 import com.ll.hotel.domain.member.admin.dto.request.AdminHotelRequest;
 import com.ll.hotel.domain.member.admin.dto.response.AdminHotelResponse;
 import com.ll.hotel.domain.member.admin.service.AdminHotelService;
@@ -19,37 +18,37 @@ public class AdminHotelController {
     private final AdminHotelService adminHotelService;
 
     @GetMapping
-    public RsData<Page<AdminHotelDto>> getAll(
+    public RsData<Page<AdminHotelResponse.Summary>> getAll(
             @RequestParam(value = "page", defaultValue = "0") int page) {
 
-        Page<AdminHotelDto> pagedAdminHotelDto = adminHotelService.findAllPaged(page)
-                .map(AdminHotelDto::toDto);
+        Page<AdminHotelResponse.Summary> pagedHotelSummaries = adminHotelService.findAllPaged(page)
+                .map(AdminHotelResponse.Summary::from);
 
-        if (!pagedAdminHotelDto.hasContent()) {
+        if (!pagedHotelSummaries.hasContent()) {
             throw new ServiceException("404", "요청하신 호텔 정보 페이지가 없습니다.");
         }
 
         return new RsData<>(
                 "200",
                 "모든 호텔 정보가 조회되었습니다.",
-                pagedAdminHotelDto
+                pagedHotelSummaries
         );
     }
 
     @GetMapping("/{id}")
-    public RsData<AdminHotelDto> getById(@PathVariable("id") Long id) {
+    public RsData<AdminHotelResponse.Detail> getById(@PathVariable("id") Long id) {
 
         Hotel hotel = adminHotelService.findById(id);
 
         return new RsData<>(
                 "200",
                 "호텔 정보가 조회되었습니다.",
-                AdminHotelDto.toDto(hotel)
+                AdminHotelResponse.Detail.from(hotel)
         );
     }
 
     @PatchMapping("/{id}")
-    public RsData<AdminHotelResponse> approve(@PathVariable("id") Long id,
+    public RsData<AdminHotelResponse.ApprovalResult> approve(@PathVariable("id") Long id,
                                               @RequestBody @Valid AdminHotelRequest adminHotelRequest) {
         Hotel hotel = adminHotelService.findById(id);
 
@@ -60,7 +59,7 @@ public class AdminHotelController {
         return new RsData<>(
                 "200",
                 "호텔 승인 정보가 수정되었습니다.",
-                new AdminHotelResponse(hotel)
+                AdminHotelResponse.ApprovalResult.from(hotel)
         );
     }
 }
