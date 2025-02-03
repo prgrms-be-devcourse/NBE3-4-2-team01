598e88a471c7b95ea531984f6f6bec8cb2d2faa9
jurio5 <sia5492@naver.com>
2025-02-03 18:21:25 +0900

    [feat] 회원 정보 엔티티 클래스 추가 및 각 ROLE 권한 설정

--- backend/src/main/java/com/ll/hotel/domain/member/member/entity/Member.java
+++ backend/src/main/java/com/ll/hotel/domain/member/member/entity/Member.java
@@ -2,13 +2,13 @@ package com.ll.hotel.domain.member.member.entity;
 
 import com.ll.hotel.domain.member.member.type.MemberStatus;
 import com.ll.hotel.global.security.oauth2.entity.OAuth;
-import com.ll.hotel.global.jpa.entity.BaseTime;
 import jakarta.persistence.*;
 import lombok.*;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
+
 
 @Entity
 @Getter
@@ -17,23 +17,35 @@ import java.util.List;
 @AllArgsConstructor
 @Builder
 @Table(name = "member")
-public class Member extends BaseTime {
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(unique = true, nullable = false)
+    private Long memberId;
 
     @Column(unique = true, nullable = false)
-    private String memberEmail; // 실제 로그인 ID
+    private String memberEmail;
 
     @Column(nullable = false)
-    private String password; // 민감한 정보에 접두사를 붙여야 할지 고민
+    private String password;
 
     @Column(nullable = false)
-    private String memberName; // 유저 닉네임
+    private String memberName;
 
     @Column(nullable = false)
     private String memberPhoneNumber;
 
     @Column(nullable = false)
     private LocalDate birthDate;
-  
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    @Column(nullable = false)
+    private LocalDateTime modifiedAt;
+
+
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
     private Role role;
@@ -44,11 +56,22 @@ public class Member extends BaseTime {
 
     @OneToMany(mappedBy = "member")
     private List<OAuth> oAuthList;
-  
-    @OneToOne(mappedBy = "member")
-    private Business business;
+
+    private String provider;
+
+    public boolean isAdmin() {
+        return this.role == Role.ADMIN;
+    }
+
+    public boolean isBusiness() {
+        return this.role == Role.BUSINESS;
+    }
+
+    public boolean isUser() {
+        return this.role == Role.USER;
+    }
 
     public String getUserRole() {
-        return this.role.name();
+        return this.role.toString();
     }
 }
