357023229401f2d16e25f6b926020ff13521592e
jurio5 <sia5492@naver.com>
2025-02-03 18:11:08 +0900

    [feat] JWT 엑세스 토큰 및 리프레시 토큰 생성 클래스 generateToken : 주어진 이메일 기반으로 액세스 토큰, 리프레시 토큰 생성하는 메서드 genAccessToken : 액세스 토큰 생성 메서드 verifyToken : 토큰 유효성 검증 메서드

---
+++ backend/src/main/java/com/ll/hotel/domain/member/member/service/AuthTokenService.java
@@ -0,0 +1,53 @@
+package com.ll.hotel.domain.member.member.service;
+
+
+import com.ll.hotel.global.security.dto.GeneratedToken;
+import com.ll.hotel.global.security.oauth2.CustomOAuth2JwtProperties;
+import com.ll.hotel.standard.util.Ut;
+import io.jsonwebtoken.Claims;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class AuthTokenService {
+    private final CustomOAuth2JwtProperties jwtProperties;
+    private final RefreshTokenService refreshTokenService;
+
+    public GeneratedToken generateToken(String email) {
+        String accessToken = genAccessToken(email);
+        String refreshToken = refreshTokenService.generateRefreshToken(email);
+
+        refreshTokenService.saveTokenInfo(email, refreshToken, accessToken);
+        return new GeneratedToken(accessToken, refreshToken);
+    }
+
+    String genAccessToken(String email) {
+        Map<String, Object> claims = new HashMap<>();
+        claims.put("sub", email);
+        claims.put("role", "ROLE_USER");
+        claims.put("type", "access");
+
+        return Ut.jwt.toString(jwtProperties, claims);
+    }
+
+    boolean verifyToken(String token) {
+        try {
+            Claims claims = Ut.jwt.getClaims(jwtProperties, token);
+            return claims.getExpiration().after(new Date());
+        } catch (Exception e) {
+            log.error("Token verification failed: {}", e.getMessage());
+            return false;
+        }
+    }
+
+    String getEmail(String token) {
+        return Ut.jwt.getClaims(jwtProperties, token).getSubject();
+    }
+}
