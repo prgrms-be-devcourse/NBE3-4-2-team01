b25f0d1a06df6e24d77df4f2f44f2cb07160ec88
jurio5 <sia5492@naver.com>
2025-02-03 17:28:27 +0900

    [feat] 현재 요청의 사용자 정보를 가져오는 클래스 추가

--- backend/src/main/java/com/ll/hotel/global/rq/Rq.java
+++ backend/src/main/java/com/ll/hotel/global/rq/Rq.java
@@ -1,4 +1,34 @@
 package com.ll.hotel.global.rq;
 
+
+import com.ll.hotel.domain.member.member.entity.Member;
+import com.ll.hotel.domain.member.member.repository.MemberRepository;
+import com.ll.hotel.global.security.dto.SecurityUser;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.annotation.RequestScope;
+
+import java.util.Optional;
+
+@Component
+@RequestScope
+@RequiredArgsConstructor
 public class Rq {
+    private final MemberRepository memberRepository;
+    private Member actor;
+
+    public Member getActor() {
+        if (actor == null) {
+            actor = Optional.ofNullable(SecurityContextHolder.getContext().getAuthentication())
+                    .map(Authentication::getPrincipal)
+                    .filter(principal -> principal instanceof SecurityUser)
+                    .map(principal -> (SecurityUser) principal)
+                    .map(securityUser -> memberRepository.findByMemberEmail(securityUser.getEmail())
+                            .orElse(null))
+                    .orElse(null);
+        }
+        return actor;
+    }
 }
