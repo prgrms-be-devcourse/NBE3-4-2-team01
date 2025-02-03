1466e976d107cd190c4e1257d3c557df00dff3b3
jurio5 <sia5492@naver.com>
2025-02-03 18:42:13 +0900

    [feat] 즐겨찾기 관련 DTO 추가

---
+++ backend/src/main/java/com/ll/hotel/domain/member/favorite/dto/FavoriteDTO.java
@@ -0,0 +1,7 @@
+package com.ll.hotel.domain.member.favorite.dto;
+
+
+public record FavoriteDTO(
+    String hotelId,
+    String memberId
+) {}
---
+++ backend/src/main/java/com/ll/hotel/domain/member/favorite/dto/FavoriteReqBody.java
@@ -0,0 +1,6 @@
+package com.ll.hotel.domain.member.favorite.dto;
+
+public record FavoriteReqBody(
+    String hotelId,
+    String memberId
+) {}
