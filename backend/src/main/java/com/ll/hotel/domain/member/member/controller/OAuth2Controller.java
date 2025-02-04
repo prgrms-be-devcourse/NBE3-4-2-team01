dbb4cc9ce1b4093424374101f71f5e07d2680436
jurio5 <sia5492@naver.com>
2025-02-04 20:24:51 +0900

    [refactor] OAuth 테이블에서 provider & OauthId 로드 되도록 변경

--- backend/src/main/java/com/ll/hotel/domain/member/member/controller/OAuth2Controller.java
+++ backend/src/main/java/com/ll/hotel/domain/member/member/controller/OAuth2Controller.java
@@ -1,22 +1,19 @@
 package com.ll.hotel.domain.member.member.controller;
 
+import com.ll.hotel.domain.member.member.entity.Member;
+import com.ll.hotel.global.rq.Rq;
+import com.ll.hotel.global.rsData.RsData;
+import com.ll.hotel.global.security.oauth2.entity.OAuth;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.ll.hotel.domain.member.member.entity.Member;
-import com.ll.hotel.domain.member.member.service.MemberService;
-import com.ll.hotel.global.rq.Rq;
-import com.ll.hotel.global.rsData.RsData;
-
-import lombok.RequiredArgsConstructor;
-
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/auth")
 public class OAuth2Controller {
-    private final MemberService memberService;
     private final Rq rq;
 
     @GetMapping("/oauth2/callback")
@@ -26,41 +23,47 @@ public class OAuth2Controller {
             @RequestParam String status
     ) {
         if ("SUCCESS".equals(status)) {
-            String token = accessToken.replace("Bearer ", "");
-            String email = memberService.getEmail(token);
-            Member member = memberService.findByMemberEmail(email)
-                    .orElseThrow(() -> new RuntimeException("회원 정보를 찾을 수 없습니다."));
-            return new RsData<>(
-                "200-1",
-                "OAuth2 로그인 성공",
-                new OAuth2Response(
-                    member.getMemberEmail(),
-                    member.getMemberName(),
-                    member.getProvider(),
-                    status,
-                    member.getUserRole(),
-                    rq.getActor().isUser(),
-                    rq.getActor().isAdmin(),
-                    rq.getActor().isBusiness(),
-                    accessToken,
-                    refreshToken
-                )
-            );
+            Member actor = rq.getActor();
+            
+            if (actor != null) {
+                OAuth oAuth = actor.getFirstOAuth();
+                
+                if (oAuth == null) {
+                    return new RsData<>("400", "OAuth 정보를 찾을 수 없습니다.", null);
+                }
+                
+                return new RsData<>(
+                    "200",
+                    "OAuth2 로그인 성공",
+                    new OAuth2Response(
+                        actor.getMemberEmail(),
+                        actor.getMemberName(),
+                        status,
+                        actor.getUserRole(),
+                        oAuth.getProvider(),
+                        oAuth.getOauthId(),
+                        actor.isUser(),
+                        actor.isAdmin(),
+                        actor.isBusiness(),
+                        accessToken,
+                        refreshToken
+                    )
+                );
+            }
+            return new RsData<>("401", "사용자를 찾을 수 없습니다.", null);
         }
         
-        return new RsData<>(
-            "400-1",
-            "잘못된 요청입니다."
-        );
+        return new RsData<>("400", "잘못된 요청입니다.", null);
     }
 }
 
 record OAuth2Response(
     String email,
     String name,
-    String provider,
     String status,
     String role,
+    String provider,
+    String oauthId,
     boolean isUser,
     boolean isAdmin,
     boolean isBusiness,
