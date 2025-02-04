d8297953fee59a7fa27a7999aba2183ba3e65be9
HJ LEE <sete3683@gmail.com>
2025-02-04 19:59:39 +0900

    [feat] 예약, 결제 데이터 검증 기능 추가
    - Request, Entity 레벨에 추가

--- backend/src/main/java/com/ll/hotel/domain/booking/payment/entity/Payment.java
+++ backend/src/main/java/com/ll/hotel/domain/booking/payment/entity/Payment.java
@@ -23,18 +23,21 @@ public class Payment extends BaseTime {
      * merchant_uid는 상점(호텔) 측에서 생성하는 주문번호
      * api key는 application.yml에 작성
      */
-    @NotNull
+    @NotNull(message = "거래 UID는 필수입니다.")
     @Column(unique = true)
     private String merchantUid;
 
+    @NotNull(message = "거래 금액은 필수입니다.")
     @Column
     private int amount;
 
+    @NotNull(message = "거래 상태 정보는 필수입니다.")
     @Enumerated(EnumType.STRING)
     @Column
     @Builder.Default
     private PaymentStatus paymentStatus = PaymentStatus.PAID;
 
+    @NotNull(message = "거래 일자는 필수입니다.")
     @Column
     private LocalDateTime paidAt;
 }
