c44c21ddcc63490473b83e3c4b33ad9b455a1865
jurio5 <sia5492@naver.com>
2025-02-04 22:42:42 +0900

    [feat] 로그아웃 시 레디스에 블랙리스트 토큰 추가 로그아웃 시 사용자의 액세스 토큰을 레디스에 블랙리스트로 등록하여, 이후 해당 토큰으로 API 요청 시 차단

--- backend/src/main/java/com/ll/hotel/domain/member/member/service/MemberService.java
+++ backend/src/main/java/com/ll/hotel/domain/member/member/service/MemberService.java
@@ -8,15 +8,19 @@ import com.ll.hotel.domain.member.member.repository.MemberRepository;
 import com.ll.hotel.domain.member.member.type.MemberStatus;
 import com.ll.hotel.global.exceptions.ServiceException;
 import com.ll.hotel.global.rsData.RsData;
+import com.ll.hotel.global.security.oauth2.CustomOAuth2JwtProperties;
 import com.ll.hotel.global.security.oauth2.entity.OAuth;
 import com.ll.hotel.global.security.oauth2.repository.OAuthRepository;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.StringUtils;
 
 import java.util.Optional;
+import java.util.concurrent.TimeUnit;
 
 @Service
 @RequiredArgsConstructor
@@ -26,7 +30,10 @@ public class MemberService {
     private final OAuthRepository oAuthRepository;
     private final AuthTokenService authTokenService;
     private final RefreshTokenService refreshTokenService;
+    private final RedisTemplate<String, String> redisTemplate;
+    private final CustomOAuth2JwtProperties jwtProperties;
     private static final Logger log = LoggerFactory.getLogger(MemberService.class);
+    private static final String LOGOUT_PREFIX = "LOGOUT:";
 
     @Transactional
     public Member join(JoinRequest joinRequest) {
@@ -94,5 +101,25 @@ public class MemberService {
 
     public String generateRefreshToken(String email) {
         return refreshTokenService.generateRefreshToken(email);
+    }
+
+    public void logout(String accessToken) {
+        if (!StringUtils.hasText(accessToken)) {
+            throw new ServiceException("400-1", "토큰이 필요합니다.");
+        }
+
+        String email = authTokenService.getEmail(accessToken);
+        
+
+        redisTemplate.opsForValue().set(
+            LOGOUT_PREFIX + accessToken,
+            email,
+            jwtProperties.getAccessTokenExpiration(),
+            TimeUnit.MILLISECONDS
+        );
+    }
+
+    public boolean isLoggedOut(String token) {
+        return Boolean.TRUE.equals(redisTemplate.hasKey(LOGOUT_PREFIX + token));
     }
 }
