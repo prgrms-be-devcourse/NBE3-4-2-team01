120ef2c31d5087356620a446f9fd6373f12b0a63
universechoi <skye.of.universe@gmail.com>
2025-02-05 12:58:16 +0900

    [fix] BusinessApiProperties에 Setter 추가하여 값 주입 가능하도록 수정

--- backend/src/main/java/com/ll/hotel/domain/member/member/type/BusinessApiProperties.java
+++ backend/src/main/java/com/ll/hotel/domain/member/member/type/BusinessApiProperties.java
@@ -1,11 +1,13 @@
 package com.ll.hotel.domain.member.member.type;
 
 import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 @Component
 @Getter
+@Setter
 @ConfigurationProperties(prefix = "business-api")
 public class BusinessApiProperties {
     private String serviceKey;
