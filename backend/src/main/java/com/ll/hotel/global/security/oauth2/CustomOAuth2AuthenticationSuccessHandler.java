83c4dae5e70e967f985af7c6c6911417063e5f0e
jurio5 <sia5492@naver.com>
2025-02-06 15:28:12 +0900

    [feat] OAuth2 기존 사용자 로그인 시 리프레쉬 토큰 쿠키에 포함하여 백에서 관리

--- backend/src/main/java/com/ll/hotel/global/security/oauth2/CustomOAuth2AuthenticationSuccessHandler.java
+++ backend/src/main/java/com/ll/hotel/global/security/oauth2/CustomOAuth2AuthenticationSuccessHandler.java
@@ -4,6 +4,7 @@ package com.ll.hotel.global.security.oauth2;
 import com.ll.hotel.domain.member.member.service.AuthTokenService;
 import com.ll.hotel.domain.member.member.service.MemberService;
 import com.ll.hotel.global.security.oauth2.dto.SecurityUser;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
@@ -55,6 +56,12 @@ public class CustomOAuth2AuthenticationSuccessHandler implements AuthenticationS
                     .build()
                     .encode()
                     .toUriString();
+            
+            Cookie refreshTokenCookie = new Cookie("refresh_token", refreshToken);
+            refreshTokenCookie.setHttpOnly(true);
+            refreshTokenCookie.setSecure(true);
+            refreshTokenCookie.setPath("/");
+            response.addCookie(refreshTokenCookie);
             
             response.sendRedirect(redirectUrl);
         }
