725c824dd4157d6b5908d8820a6a9e4011e0e08f
LeeKyoWon <59869849+LeeKyoWon@users.noreply.github.com>
2025-02-07 21:00:58 +0900

    Revert "[FEAT] [BE] 예약 api 엔드포인트 변경/추가"

--- backend/src/main/java/com/ll/hotel/domain/booking/booking/controller/BookingController.java
+++ backend/src/main/java/com/ll/hotel/domain/booking/booking/controller/BookingController.java
@@ -4,7 +4,6 @@ import com.ll.hotel.domain.booking.booking.dto.BookingRequest;
 import com.ll.hotel.domain.booking.booking.dto.BookingResponse;
 import com.ll.hotel.domain.booking.booking.entity.Booking;
 import com.ll.hotel.domain.booking.booking.service.BookingService;
-import com.ll.hotel.domain.hotel.hotel.entity.Hotel;
 import com.ll.hotel.domain.member.member.entity.Member;
 import com.ll.hotel.global.exceptions.ServiceException;
 import com.ll.hotel.global.rq.Rq;
@@ -89,10 +88,11 @@ public class BookingController {
         );
     }
 
-    // 호텔(사업자)측 예약 조회
-    @GetMapping("/myHotel")
+    // 호텔측 예약 조회
+    @GetMapping("/hotels/{hotel_id}")
     @Transactional
     public RsData<PageDto<BookingResponse>> getHotelBookings(
+            @PathVariable("hotel_id") Long hotelId,
             @RequestParam(defaultValue = "1", name = "page") int page,
             @RequestParam(defaultValue = "5", name = "page_size") int pageSize) {
         Member actor = rq.getActor();
@@ -102,18 +102,11 @@ public class BookingController {
             throw new ServiceException("401", "예약 조회 권한이 없습니다.");
         }
 
-        Hotel myHotel = actor.getBusiness().getHotel();
-
-        // 내 호텔이 없을 경우
-        if (myHotel == null) {
-            throw new ServiceException("404", "등록된 호텔이 없습니다.");
-        }
-
         return new RsData<>(
                 "200",
                 "호텔 예약 목록 조회에 성공했습니다.",
                 new PageDto<>(
-                        bookingService.findByHotel(myHotel, page, pageSize)
+                        bookingService.findByHotelId(hotelId, page, pageSize)
                                 .map(BookingResponse::from)
                 )
         );
@@ -157,28 +150,6 @@ public class BookingController {
         return new RsData<>(
                 "200",
                 "예약 및 결제가 취소되었습니다.",
-                BookingResponse.from(booking)
-        );
-    }
-
-    // 예약(숙박) 완료 처리
-    @PostMapping("/{booking_id}")
-    @Transactional
-    public RsData<BookingResponse> complete(
-            @PathVariable("booking_id") Long bookingId) {
-        Member actor = rq.getActor();
-        Booking booking = bookingService.findById(bookingId);
-
-        // 관리자, 호텔 사업자만 완료 처리 가능
-        if (actor == null || !(actor.isAdmin() || booking.isOwnedBy(actor))) {
-            throw new ServiceException("401", "예약 완료 처리 권한이 없습니다.");
-        }
-
-        booking = bookingService.setCompleted(booking);
-
-        return new RsData<>(
-                "200",
-                "예약이 완료 처리되었습니다.",
                 BookingResponse.from(booking)
         );
     }
