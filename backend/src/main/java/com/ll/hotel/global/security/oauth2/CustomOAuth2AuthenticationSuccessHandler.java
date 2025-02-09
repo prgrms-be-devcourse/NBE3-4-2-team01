bd7ba61db2d2c3f1f20dec1dd855ae5422e04f13
LeeKyoWon <kskw20@naver.com>
2025-02-09 13:07:08 +0900

    CustomOAuth2AuthenticationSuccessHandler 자동 import 순서 변경

--- backend/src/main/java/com/ll/hotel/global/security/oauth2/CustomOAuth2AuthenticationSuccessHandler.java
+++ backend/src/main/java/com/ll/hotel/global/security/oauth2/CustomOAuth2AuthenticationSuccessHandler.java
@@ -1,26 +1,24 @@
 package com.ll.hotel.global.security.oauth2;
 
 
-import java.io.IOException;
-import java.net.URLEncoder;
-import java.nio.charset.StandardCharsets;
-
+import com.ll.hotel.domain.member.member.entity.Member;
+import com.ll.hotel.domain.member.member.service.AuthTokenService;
+import com.ll.hotel.domain.member.member.service.MemberService;
+import com.ll.hotel.global.security.oauth2.dto.SecurityUser;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 import org.springframework.web.util.UriComponentsBuilder;
 
-import com.ll.hotel.domain.member.member.entity.Member;
-import com.ll.hotel.domain.member.member.service.AuthTokenService;
-import com.ll.hotel.domain.member.member.service.MemberService;
-import com.ll.hotel.global.security.oauth2.dto.SecurityUser;
-
-import jakarta.servlet.http.Cookie;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
+import java.io.IOException;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 
 @Slf4j
 @Component
