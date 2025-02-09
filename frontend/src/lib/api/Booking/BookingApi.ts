2c254d0f4feb4de673de752ec3c17894a351e9da
HJ LEE <sete3683@gmail.com>
2025-02-10 04:12:20 +0900

    [feat] [FE] 엑세스 토큰 적용
    - slack에 공유된 내용에 맞춰 엑세스 토큰 적용

--- frontend/src/lib/api/Booking/BookingApi.ts
+++ frontend/src/lib/api/Booking/BookingApi.ts
@@ -8,37 +8,13 @@ export const book = async function(bookingRequest : BookingRequest) : Promise<Bo
     try {
         const response = await fetch("http://localhost:8080/api/bookings", {
             method: "POST",
+            credentials: 'include',
             headers: {
                 "Content-Type": "application/json",
             },
             body: JSON.stringify(bookingRequest)
         });
 
-        const rsData = await response.json();
-
-        if (rsData.resultCode.startsWith("2")) {
-            return rsData.data;
-        } else {
-            throw new Error(`에러 발생 : ${rsData.resultCode}, ${rsData.msg}`);
-        }
-    } catch (error) {
-        throw error;
-    }
-};
-
-// 전체 예약 조회 (관리자측, 미사용)
-export const getAllBookings = async function(page? : number, pageSize? : number) : Promise<PageDto<BookingResponseSummary>> {
-    try {
-        const params = new URLSearchParams();
-
-        if (page) {
-            params.append("page", page.toString());
-        }
-        if (pageSize) {
-            params.append("page_size", pageSize.toString());
-        }
-
-        const response = await fetch(`http://localhost:8080/api/bookings?${params.toString()}`);
         const rsData = await response.json();
 
         if (rsData.resultCode.startsWith("2")) {
@@ -63,7 +39,9 @@ export const getMyBookings = async function(page? : number, pageSize? : number) 
             params.append("page_size", pageSize.toString());
         }
 
-        const response = await fetch(`http://localhost:8080/api/bookings/me?${params.toString()}`);
+        const response = await fetch(`http://localhost:8080/api/bookings/me?${params.toString()}`, {
+            credentials: 'include',
+        });
         const rsData = await response.json();
 
         if (rsData.resultCode.startsWith("2")) {
@@ -88,7 +66,9 @@ export const getHotelBookings = async function(page? : number, pageSize? : numbe
             params.append("page_size", pageSize.toString());
         }
 
-        const response = await fetch(`http://localhost:8080/api/bookings/myHotel?${params.toString()}`);
+        const response = await fetch(`http://localhost:8080/api/bookings/myHotel?${params.toString()}`, {
+            credentials: 'include',
+        });
         const rsData = await response.json();
 
         if (rsData.resultCode.startsWith("2")) {
@@ -104,7 +84,9 @@ export const getHotelBookings = async function(page? : number, pageSize? : numbe
 // 예약 상세 조회
 export const getBookingDetails = async function(bookingId : number) : Promise<BookingResponseDetails> {
     try {
-        const response = await fetch(`http://localhost:8080/api/bookings/${bookingId}`);
+        const response = await fetch(`http://localhost:8080/api/bookings/${bookingId}`, {
+            credentials: 'include',
+        });
         const rsData = await response.json();
 
         if (rsData.resultCode.startsWith("2")) {
@@ -121,7 +103,8 @@ export const getBookingDetails = async function(bookingId : number) : Promise<Bo
 export const cancelBooking = async function(bookingId : number) : Promise<BookingResponseDetails> {
     try {
         const response = await fetch(`http://localhost:8080/api/bookings/${bookingId}`, {
-            method: "DELETE"
+            method: "DELETE",
+            credentials: 'include',
         });
 
         const rsData = await response.json();
@@ -140,7 +123,8 @@ export const cancelBooking = async function(bookingId : number) : Promise<Bookin
 export const completeBooking = async function(bookingId : number) : Promise<BookingResponseDetails> {
     try {
         const response = await fetch(`http://localhost:8080/api/bookings/${bookingId}`, {
-            method: "PATCH"
+            method: "POST",
+            credentials: 'include',
         });
 
         const rsData = await response.json();
