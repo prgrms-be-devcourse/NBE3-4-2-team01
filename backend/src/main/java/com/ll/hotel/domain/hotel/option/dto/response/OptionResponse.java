b90447787310e9db652b2ddfc576c30c6360d6d0
universechoi <skye.of.universe@gmail.com>
2025-02-10 12:43:15 +0900

    [refactor] [BE] 응답 속성 이름을 id에서 optionId로 변경

--- backend/src/main/java/com/ll/hotel/domain/hotel/option/dto/response/OptionResponse.java
+++ backend/src/main/java/com/ll/hotel/domain/hotel/option/dto/response/OptionResponse.java
@@ -2,7 +2,7 @@ package com.ll.hotel.domain.hotel.option.dto.response;
 
 import com.ll.hotel.domain.hotel.option.entity.BaseOption;
 
-public record OptionResponse(Long id, String name) {
+public record OptionResponse(Long optionId, String name) {
     public static OptionResponse from(BaseOption option) {
         return new OptionResponse(option.getId(), option.getName());
     }
