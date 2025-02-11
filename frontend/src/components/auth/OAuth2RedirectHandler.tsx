12489884dcb915775f8260b9ed765bf644c9c7e4
LeeKyoWon <kskw20@naver.com>
2025-02-11 15:22:49 +0900

    [Feat][FE] 관리자 메인페이지 구현

--- frontend/src/components/auth/OAuth2RedirectHandler.tsx
+++ frontend/src/components/auth/OAuth2RedirectHandler.tsx
@@ -1,6 +1,7 @@
-'use client';
-import { useEffect } from 'react';
-import { useRouter } from 'next/navigation';
+"use client";
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+import { getRoleFromCookie } from "@/lib/utils/CookieUtil";
 
 interface OAuth2RedirectProps {
   onLoginSuccess?: () => void;
@@ -12,34 +13,49 @@ const OAuth2RedirectHandler = ({ onLoginSuccess }: OAuth2RedirectProps) => {
   useEffect(() => {
     const handleOAuth2Redirect = async () => {
       const searchParams = new URLSearchParams(window.location.search);
-      const status = searchParams.get('status');
-      
+      const status = searchParams.get("status");
+
       try {
         switch (status) {
-          case 'REGISTER': {
-            const provider = searchParams.get('provider');
-            const oauthId = searchParams.get('oauthId');
-            
+          case "REGISTER": {
+            const provider = searchParams.get("provider");
+            const oauthId = searchParams.get("oauthId");
+
             if (!provider || !oauthId) {
-                throw new Error('OAuth 정보가 누락되었습니다.');
+              throw new Error("OAuth 정보가 누락되었습니다.");
             }
-            
-            router.push(`/join?provider=${encodeURIComponent(provider)}&oauthId=${encodeURIComponent(oauthId)}`);
+
+            router.push(
+              `/join?provider=${encodeURIComponent(
+                provider
+              )}&oauthId=${encodeURIComponent(oauthId)}`
+            );
             break;
           }
-          
-          case 'SUCCESS': {
+
+          case "SUCCESS": {
             onLoginSuccess?.();
-            router.push('/');
+            const roleData = getRoleFromCookie();
+            if (roleData?.role === "ADMIN") {
+              router.push("/admin");
+              break;
+            }
+            router.push("/");
             break;
           }
-          
+
           default:
-            throw new Error('잘못된 인증 상태입니다.');
+            throw new Error("잘못된 인증 상태입니다.");
         }
       } catch (error) {
-        console.error('OAuth2 리다이렉트 처리 중 오류 발생:', error);
-        router.push(`/login?error=${encodeURIComponent(error instanceof Error ? error.message : '인증 처리 중 오류가 발생했습니다.')}`);
+        console.error("OAuth2 리다이렉트 처리 중 오류 발생:", error);
+        router.push(
+          `/login?error=${encodeURIComponent(
+            error instanceof Error
+              ? error.message
+              : "인증 처리 중 오류가 발생했습니다."
+          )}`
+        );
       }
     };
 
