c5d3db3a7919a4a048359d3f809f828f0f28ccdd
LeeKyoWon <kskw20@naver.com>
2025-03-05 17:19:43 +0900

    [Fix][BE] Empty 클래스, 이에 대한 의존성 제거

--- backend/src/main/java/com/ll/hotel/domain/member/member/controller/FavoriteController.java
+++ backend/src/main/java/com/ll/hotel/domain/member/member/controller/FavoriteController.java
@@ -1,20 +1,13 @@
 package com.ll.hotel.domain.member.member.controller;
 
-import java.util.List;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 import com.ll.hotel.domain.hotel.hotel.dto.HotelDto;
 import com.ll.hotel.domain.member.member.service.MemberService;
 import com.ll.hotel.global.rsData.RsData;
-
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -23,15 +16,15 @@ public class FavoriteController {
     private final MemberService memberService;
 
     @PostMapping("/{hotelId}")
-    public RsData<?> addFavorite(@PathVariable Long hotelId) {
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void addFavorite(@PathVariable Long hotelId) {
         memberService.addFavorite(hotelId);
-        return RsData.success(HttpStatus.OK, "즐겨찾기에 추가되었습니다.");
     }
 
     @DeleteMapping("/{hotelId}")
-    public RsData<?> removeFavorite(@PathVariable Long hotelId) {
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void removeFavorite(@PathVariable Long hotelId) {
         memberService.removeFavorite(hotelId);
-        return RsData.success(HttpStatus.OK, "즐겨찾기가 삭제되었습니다.");
     }
 
     @GetMapping("/me")
