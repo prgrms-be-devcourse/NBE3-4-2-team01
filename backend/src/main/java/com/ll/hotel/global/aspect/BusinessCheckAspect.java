b6547c43c143dec1eebc491f96fdbba043521cae
Gyeonghun Min <riverpower6@naver.com>
2025-03-07 15:28:57 +0900

    [REFACTOR] [BE] 중복 에러 로그 저장 문제 수정

--- backend/src/main/java/com/ll/hotel/global/aspect/BusinessCheckAspect.java
+++ backend/src/main/java/com/ll/hotel/global/aspect/BusinessCheckAspect.java
@@ -4,9 +4,9 @@ import com.ll.hotel.domain.member.member.entity.Member;
 import com.ll.hotel.standard.util.LogUtil;
 import java.util.Arrays;
 import lombok.extern.slf4j.Slf4j;
-import org.aspectj.lang.ProceedingJoinPoint;
-import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
 
 @Aspect
@@ -14,16 +14,14 @@ import org.springframework.stereotype.Component;
 @Component
 public class BusinessCheckAspect {
 
-    @Around("@annotation(com.ll.hotel.global.annotation.BusinessOnly)")
-    public Object checkBusiness(ProceedingJoinPoint joinPoint) throws Throwable {
+    @Before("@annotation(com.ll.hotel.global.annotation.BusinessOnly)")
+    public void checkBusiness(JoinPoint joinPoint) {
         try {
             Arrays.stream(joinPoint.getArgs()).forEach(arg -> {
                 if (arg instanceof Member member) {
                     member.checkBusiness();
                 }
             });
-
-            return joinPoint.proceed();
         } catch (Throwable ex) {
             LogUtil.logError(log, joinPoint, ex);
             throw ex;
